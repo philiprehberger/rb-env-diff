7c97a601e6ba10a7ce736788a08fac71190409fe
Philip Rehberger <me@philiprehberger.com>
2021-02-17 23:55:49 -0600

    Standardize RuboCop config and auto-correct offenses

--- spec/philiprehberger/env_diff_spec.rb
+++ spec/philiprehberger/env_diff_spec.rb
@@ -1,171 +1,171 @@
 # frozen_string_literal: true
 
-require "spec_helper"
-require "tempfile"
+require 'spec_helper'
+require 'tempfile'
 
 RSpec.describe Philiprehberger::EnvDiff do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Philiprehberger::EnvDiff::VERSION).not_to be_nil
   end
 
-  describe ".compare" do
-    it "detects added keys" do
-      diff = described_class.compare({ "A" => "1" }, { "A" => "1", "B" => "2" })
-      expect(diff.added).to eq(["B"])
+  describe '.compare' do
+    it 'detects added keys' do
+      diff = described_class.compare({ 'A' => '1' }, { 'A' => '1', 'B' => '2' })
+      expect(diff.added).to eq(['B'])
     end
 
-    it "detects removed keys" do
-      diff = described_class.compare({ "A" => "1", "B" => "2" }, { "A" => "1" })
-      expect(diff.removed).to eq(["B"])
+    it 'detects removed keys' do
+      diff = described_class.compare({ 'A' => '1', 'B' => '2' }, { 'A' => '1' })
+      expect(diff.removed).to eq(['B'])
     end
 
-    it "detects changed keys" do
-      diff = described_class.compare({ "A" => "1" }, { "A" => "2" })
-      expect(diff.changed).to eq({ "A" => { source: "1", target: "2" } })
+    it 'detects changed keys' do
+      diff = described_class.compare({ 'A' => '1' }, { 'A' => '2' })
+      expect(diff.changed).to eq({ 'A' => { source: '1', target: '2' } })
     end
 
-    it "detects unchanged keys" do
-      diff = described_class.compare({ "A" => "1", "B" => "2" }, { "A" => "1", "B" => "3" })
-      expect(diff.unchanged).to eq(["A"])
+    it 'detects unchanged keys' do
+      diff = described_class.compare({ 'A' => '1', 'B' => '2' }, { 'A' => '1', 'B' => '3' })
+      expect(diff.unchanged).to eq(['A'])
     end
 
-    it "handles all difference types together" do
-      source = { "KEEP" => "same", "CHANGE" => "old", "REMOVE" => "bye" }
-      target = { "KEEP" => "same", "CHANGE" => "new", "ADD" => "hello" }
+    it 'handles all difference types together' do
+      source = { 'KEEP' => 'same', 'CHANGE' => 'old', 'REMOVE' => 'bye' }
+      target = { 'KEEP' => 'same', 'CHANGE' => 'new', 'ADD' => 'hello' }
       diff = described_class.compare(source, target)
 
-      expect(diff.added).to eq(["ADD"])
-      expect(diff.removed).to eq(["REMOVE"])
-      expect(diff.changed).to eq({ "CHANGE" => { source: "old", target: "new" } })
-      expect(diff.unchanged).to eq(["KEEP"])
+      expect(diff.added).to eq(['ADD'])
+      expect(diff.removed).to eq(['REMOVE'])
+      expect(diff.changed).to eq({ 'CHANGE' => { source: 'old', target: 'new' } })
+      expect(diff.unchanged).to eq(['KEEP'])
     end
   end
 
   describe Philiprehberger::EnvDiff::Diff do
-    describe "#changed?" do
-      it "returns true when there are differences" do
-        diff = described_class.new({ "A" => "1" }, { "B" => "2" })
+    describe '#changed?' do
+      it 'returns true when there are differences' do
+        diff = described_class.new({ 'A' => '1' }, { 'B' => '2' })
         expect(diff.changed?).to be true
       end
 
-      it "returns false when hashes are identical" do
-        diff = described_class.new({ "A" => "1" }, { "A" => "1" })
+      it 'returns false when hashes are identical' do
+        diff = described_class.new({ 'A' => '1' }, { 'A' => '1' })
         expect(diff.changed?).to be false
       end
 
-      it "returns false for empty hashes" do
+      it 'returns false for empty hashes' do
         diff = described_class.new({}, {})
         expect(diff.changed?).to be false
       end
     end
 
-    describe "#summary" do
+    describe '#summary' do
       it "returns 'No differences found.' for identical hashes" do
-        diff = described_class.new({ "A" => "1" }, { "A" => "1" })
-        expect(diff.summary).to eq("No differences found.")
+        diff = described_class.new({ 'A' => '1' }, { 'A' => '1' })
+        expect(diff.summary).to eq('No differences found.')
       end
 
-      it "includes added, removed, and changed keys" do
-        source = { "KEEP" => "same", "CHANGE" => "old", "REMOVE" => "bye" }
-        target = { "KEEP" => "same", "CHANGE" => "new", "ADD" => "hello" }
+      it 'includes added, removed, and changed keys' do
+        source = { 'KEEP' => 'same', 'CHANGE' => 'old', 'REMOVE' => 'bye' }
+        target = { 'KEEP' => 'same', 'CHANGE' => 'new', 'ADD' => 'hello' }
         diff = described_class.new(source, target)
         summary = diff.summary
 
-        expect(summary).to include("+ ADD")
-        expect(summary).to include("- REMOVE")
-        expect(summary).to include("~ CHANGE")
+        expect(summary).to include('+ ADD')
+        expect(summary).to include('- REMOVE')
+        expect(summary).to include('~ CHANGE')
       end
 
-      it "returns no-diff message for empty hashes" do
+      it 'returns no-diff message for empty hashes' do
         diff = described_class.new({}, {})
-        expect(diff.summary).to eq("No differences found.")
+        expect(diff.summary).to eq('No differences found.')
       end
     end
   end
 
   describe Philiprehberger::EnvDiff::Parser do
-    describe ".parse" do
-      it "parses simple KEY=VALUE pairs" do
+    describe '.parse' do
+      it 'parses simple KEY=VALUE pairs' do
         result = described_class.parse("FOO=bar\nBAZ=qux")
-        expect(result).to eq({ "FOO" => "bar", "BAZ" => "qux" })
+        expect(result).to eq({ 'FOO' => 'bar', 'BAZ' => 'qux' })
       end
 
-      it "ignores comments" do
+      it 'ignores comments' do
         result = described_class.parse("# this is a comment\nFOO=bar")
-        expect(result).to eq({ "FOO" => "bar" })
+        expect(result).to eq({ 'FOO' => 'bar' })
       end
 
-      it "ignores blank lines" do
+      it 'ignores blank lines' do
         result = described_class.parse("FOO=bar\n\n\nBAZ=qux")
-        expect(result).to eq({ "FOO" => "bar", "BAZ" => "qux" })
+        expect(result).to eq({ 'FOO' => 'bar', 'BAZ' => 'qux' })
       end
 
-      it "handles double-quoted values" do
+      it 'handles double-quoted values' do
         result = described_class.parse('FOO="hello world"')
-        expect(result).to eq({ "FOO" => "hello world" })
+        expect(result).to eq({ 'FOO' => 'hello world' })
       end
 
-      it "handles single-quoted values" do
+      it 'handles single-quoted values' do
         result = described_class.parse("FOO='hello world'")
-        expect(result).to eq({ "FOO" => "hello world" })
+        expect(result).to eq({ 'FOO' => 'hello world' })
       end
 
-      it "handles export prefix" do
-        result = described_class.parse("export FOO=bar")
-        expect(result).to eq({ "FOO" => "bar" })
+      it 'handles export prefix' do
+        result = described_class.parse('export FOO=bar')
+        expect(result).to eq({ 'FOO' => 'bar' })
       end
 
-      it "handles empty values" do
-        result = described_class.parse("FOO=")
-        expect(result).to eq({ "FOO" => "" })
+      it 'handles empty values' do
+        result = described_class.parse('FOO=')
+        expect(result).to eq({ 'FOO' => '' })
       end
     end
 
-    describe ".parse_file" do
-      it "reads and parses a file" do
-        file = Tempfile.new(".env")
+    describe '.parse_file' do
+      it 'reads and parses a file' do
+        file = Tempfile.new('.env')
         file.write("FOO=bar\nBAZ=qux")
         file.close
 
         result = described_class.parse_file(path: file.path)
-        expect(result).to eq({ "FOO" => "bar", "BAZ" => "qux" })
+        expect(result).to eq({ 'FOO' => 'bar', 'BAZ' => 'qux' })
       ensure
         file&.unlink
       end
     end
   end
 
-  describe ".from_hash" do
-    it "compares two hashes" do
-      diff = described_class.from_hash({ "A" => "1" }, { "A" => "2" })
+  describe '.from_hash' do
+    it 'compares two hashes' do
+      diff = described_class.from_hash({ 'A' => '1' }, { 'A' => '2' })
       expect(diff.changed?).to be true
-      expect(diff.changed).to eq({ "A" => { source: "1", target: "2" } })
+      expect(diff.changed).to eq({ 'A' => { source: '1', target: '2' } })
     end
   end
 
-  describe ".from_env_file" do
-    it "parses and compares two env files" do
-      file_a = Tempfile.new(".env.a")
+  describe '.from_env_file' do
+    it 'parses and compares two env files' do
+      file_a = Tempfile.new('.env.a')
       file_a.write("FOO=bar\nOLD=val")
       file_a.close
 
-      file_b = Tempfile.new(".env.b")
+      file_b = Tempfile.new('.env.b')
       file_b.write("FOO=baz\nNEW=val")
       file_b.close
 
       diff = described_class.from_env_file(file_a.path, file_b.path)
-      expect(diff.added).to eq(["NEW"])
-      expect(diff.removed).to eq(["OLD"])
-      expect(diff.changed).to eq({ "FOO" => { source: "bar", target: "baz" } })
+      expect(diff.added).to eq(['NEW'])
+      expect(diff.removed).to eq(['OLD'])
+      expect(diff.changed).to eq({ 'FOO' => { source: 'bar', target: 'baz' } })
     ensure
       file_a&.unlink
       file_b&.unlink
     end
   end
 
-  describe ".compare edge cases" do
-    it "returns no differences for identical environments" do
-      env = { "A" => "1", "B" => "2" }
+  describe '.compare edge cases' do
+    it 'returns no differences for identical environments' do
+      env = { 'A' => '1', 'B' => '2' }
       diff = described_class.compare(env, env.dup)
       expect(diff.changed?).to be false
       expect(diff.added).to be_empty
@@ -174,94 +174,94 @@ RSpec.describe Philiprehberger::EnvDiff do
       expect(diff.unchanged).to eq(%w[A B])
     end
 
-    it "handles both environments empty" do
+    it 'handles both environments empty' do
       diff = described_class.compare({}, {})
       expect(diff.changed?).to be false
       expect(diff.unchanged).to be_empty
     end
 
-    it "handles source empty and target full" do
-      diff = described_class.compare({}, { "A" => "1", "B" => "2" })
+    it 'handles source empty and target full' do
+      diff = described_class.compare({}, { 'A' => '1', 'B' => '2' })
       expect(diff.added).to eq(%w[A B])
       expect(diff.removed).to be_empty
       expect(diff.changed).to be_empty
     end
 
-    it "handles source full and target empty" do
-      diff = described_class.compare({ "A" => "1", "B" => "2" }, {})
+    it 'handles source full and target empty' do
+      diff = described_class.compare({ 'A' => '1', 'B' => '2' }, {})
       expect(diff.added).to be_empty
       expect(diff.removed).to eq(%w[A B])
       expect(diff.changed).to be_empty
     end
 
-    it "treats keys as case-sensitive" do
-      diff = described_class.compare({ "foo" => "1" }, { "FOO" => "1" })
-      expect(diff.removed).to eq(["foo"])
-      expect(diff.added).to eq(["FOO"])
+    it 'treats keys as case-sensitive' do
+      diff = described_class.compare({ 'foo' => '1' }, { 'FOO' => '1' })
+      expect(diff.removed).to eq(['foo'])
+      expect(diff.added).to eq(['FOO'])
     end
 
-    it "detects value-only whitespace differences" do
-      diff = described_class.compare({ "A" => "hello" }, { "A" => "hello " })
-      expect(diff.changed).to eq({ "A" => { source: "hello", target: "hello " } })
+    it 'detects value-only whitespace differences' do
+      diff = described_class.compare({ 'A' => 'hello' }, { 'A' => 'hello ' })
+      expect(diff.changed).to eq({ 'A' => { source: 'hello', target: 'hello ' } })
     end
 
-    it "handles empty string values" do
-      diff = described_class.compare({ "A" => "" }, { "A" => "" })
+    it 'handles empty string values' do
+      diff = described_class.compare({ 'A' => '' }, { 'A' => '' })
       expect(diff.changed?).to be false
-      expect(diff.unchanged).to eq(["A"])
+      expect(diff.unchanged).to eq(['A'])
     end
 
-    it "detects empty vs non-empty value" do
-      diff = described_class.compare({ "A" => "" }, { "A" => "val" })
-      expect(diff.changed).to eq({ "A" => { source: "", target: "val" } })
+    it 'detects empty vs non-empty value' do
+      diff = described_class.compare({ 'A' => '' }, { 'A' => 'val' })
+      expect(diff.changed).to eq({ 'A' => { source: '', target: 'val' } })
     end
   end
 
   describe Philiprehberger::EnvDiff::Diff do
-    describe "#summary edge cases" do
-      it "formats multiple added keys" do
-        diff = described_class.new({}, { "X" => "1", "Y" => "2", "Z" => "3" })
+    describe '#summary edge cases' do
+      it 'formats multiple added keys' do
+        diff = described_class.new({}, { 'X' => '1', 'Y' => '2', 'Z' => '3' })
         summary = diff.summary
-        expect(summary).to include("+ X")
-        expect(summary).to include("+ Y")
-        expect(summary).to include("+ Z")
+        expect(summary).to include('+ X')
+        expect(summary).to include('+ Y')
+        expect(summary).to include('+ Z')
       end
 
-      it "formats multiple removed keys" do
-        diff = described_class.new({ "X" => "1", "Y" => "2" }, {})
+      it 'formats multiple removed keys' do
+        diff = described_class.new({ 'X' => '1', 'Y' => '2' }, {})
         summary = diff.summary
-        expect(summary).to include("- X")
-        expect(summary).to include("- Y")
+        expect(summary).to include('- X')
+        expect(summary).to include('- Y')
       end
 
-      it "includes old and new values in changed line" do
-        diff = described_class.new({ "PORT" => "3000" }, { "PORT" => "8080" })
-        expect(diff.summary).to include("3000")
-        expect(diff.summary).to include("8080")
+      it 'includes old and new values in changed line' do
+        diff = described_class.new({ 'PORT' => '3000' }, { 'PORT' => '8080' })
+        expect(diff.summary).to include('3000')
+        expect(diff.summary).to include('8080')
       end
     end
   end
 
   describe Philiprehberger::EnvDiff::Parser do
-    describe ".parse edge cases" do
-      it "handles values with equals signs" do
-        result = described_class.parse("URL=http://example.com?a=1&b=2")
-        expect(result).to eq({ "URL" => "http://example.com?a=1&b=2" })
+    describe '.parse edge cases' do
+      it 'handles values with equals signs' do
+        result = described_class.parse('URL=http://example.com?a=1&b=2')
+        expect(result).to eq({ 'URL' => 'http://example.com?a=1&b=2' })
       end
 
-      it "handles export with quoted value" do
+      it 'handles export with quoted value' do
         result = described_class.parse('export FOO="bar baz"')
-        expect(result).to eq({ "FOO" => "bar baz" })
+        expect(result).to eq({ 'FOO' => 'bar baz' })
       end
 
-      it "parses content with only comments and blank lines" do
+      it 'parses content with only comments and blank lines' do
         result = described_class.parse("# comment\n\n# another\n")
         expect(result).to eq({})
       end
 
-      it "handles keys with underscores and numbers" do
-        result = described_class.parse("MY_VAR_2=value")
-        expect(result).to eq({ "MY_VAR_2" => "value" })
+      it 'handles keys with underscores and numbers' do
+        result = described_class.parse('MY_VAR_2=value')
+        expect(result).to eq({ 'MY_VAR_2' => 'value' })
       end
     end
   end
